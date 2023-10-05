5503c1d339b109db1590b976e2bbc7d9b062a357
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-05 20:34:21 +0800

    show product

--- Products/SearchedProduct.js
+++ Products/SearchedProduct.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Dimensions } from 'react-native'
+import { View, StyleSheet, Dimensions, FlatList, TouchableOpacity } from 'react-native'
 import {
     Container,
     VStack,
@@ -12,55 +12,58 @@ import {
     Avatar,
     Spacer,
 } from "native-base";
-import { FlatList } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
 
 var { width } = Dimensions.get("window")
 
 const SearchedProduct = (props) => {
     const { productsFiltered } = props;
+    const navigation = useNavigation();
     return (
-
         <Container style={{ width: width }}>
             {productsFiltered.length > 0 ? (
-                // productsFiltered.map((item) => (
-                //      console.log(item)
-
-
-                // ))
                 <Box width={80}>
                     <Heading fontSize="xl" p="4" pb="3">
                         Products
                     </Heading>
                     <FlatList data={productsFiltered} renderItem={({ item }) =>
-                        <Box borderBottomWidth="1" _dark={{
-                            borderColor: "muted.50"
-                        }} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
-                            <HStack space={[2, 3]} justifyContent="space-between">
-                                <Avatar size="48px" source={{
-                                    uri: item.image ?
-                                        item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
-                                }} />
-                                <VStack>
-                                    <Text _dark={{
+                        <TouchableOpacity onPress={() => {
+                            navigation.navigate("Product Detail", { item: item })
+                        }}>
+                            <Box borderBottomWidth="1" _dark={{
+                                borderColor: "muted.50"
+                            }} borderColor="muted.800" pl={["0", "4"]} pr={["0", "5"]} py="2">
+                                <HStack space={[2, 3]} justifyContent="space-between">
+                                    <Avatar size="48px" source={{
+                                        uri: item.image ?
+                                            item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                                    }} />
+                                    <VStack>
+                                        <Text _dark={{
+                                            color: "warmGray.50"
+                                        }} color="coolGray.800" bold>
+                                            {item.name}
+                                        </Text>
+                                        <Text color="coolGray.600" _dark={{
+                                            color: "warmGray.200"
+                                        }}>
+                                            {item.description}
+                                        </Text>
+                                    </VStack>
+                                    <Spacer />
+                                    <Text fontSize="xs" _dark={{
                                         color: "warmGray.50"
-                                    }} color="coolGray.800" bold>
-                                        {item.name}
+                                    }} color="coolGray.800" alignSelf="flex-start">
+                                        {item.price}
                                     </Text>
-                                    <Text color="coolGray.600" _dark={{
-                                        color: "warmGray.200"
-                                    }}>
-                                        {item.description}
-                                    </Text>
-                                </VStack>
-                                <Spacer />
-                                <Text fontSize="xs" _dark={{
-                                    color: "warmGray.50"
-                                }} color="coolGray.800" alignSelf="flex-start">
-                                    {item.price}
-                                </Text>
-                            </HStack>
-                        </Box>} keyExtractor={item => item._id} />
+                                </HStack>
+                            </Box>
+                        </TouchableOpacity>}
+
+                        />      
                 </Box>
+
             ) : (
                 <View style={styles.center}>
                     <Text style={{ alignSelf: 'center' }}>
@@ -72,7 +75,6 @@ const SearchedProduct = (props) => {
 
     );
 };
-
 
 const styles = StyleSheet.create({
     center: {
