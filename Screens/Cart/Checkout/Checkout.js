7e74e86b59337c4f150aedf72cb21bd273ff8e7d
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-08 19:27:56 +0800

    shipping address

--- Screens/Cart/Checkout/Checkout.js
+++ Screens/Cart/Checkout/Checkout.js
@@ -1,11 +1,116 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useEffect, useState, useContext } from 'react'
+import { Text, View, Button } from 'react-native'
+import { Select, Item, Picker, Toast } from 'native-base'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import FormContainer from '../../../Shared/Form/FormContainer'
+import Input from '../../../Shared/Form/Input'
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
-const Checkout = () => {
+import { useSelector } from 'react-redux'
+import { useNavigation } from '@react-navigation/native';
+
+const countries = require("../../../assets/countries.json");
+
+const Checkout = (props) => {
+    const [orderItems, setOrderItems] = useState([])
+    const [address, setAddress] = useState('')
+    const [address2, setAddress2] = useState('')
+    const [city, setCity] = useState('')
+    const [zip, setZip] = useState('')
+    const [country, setCountry] = useState('')
+    const [phone, setPhone] = useState('')
+
+    const navigation = useNavigation()
+    const cartItems = useSelector(state => state.cartItems)
+
+    useEffect(() => {
+        setOrderItems(cartItems)
+        return () => {
+            setOrderItems();
+        }
+    }, [])
+
+    const checkOut = () => {
+        console.log("orders", orderItems)
+        let order = {
+            city,
+            country,
+            dateOrdered: Date.now(),
+            orderItems,
+            phone,
+            shippingAddress1: address,
+            shippingAddress2: address2,
+            status: "3",
+            // user,
+            zip,
+        }
+        console.log("ship", order)
+        navigation.navigate("Payment", { order: order })
+    }
+    console.log(orderItems)
     return (
-        <View>
-            <Text>Checkout</Text>
-        </View>
+        <KeyboardAwareScrollView
+            viewIsInsideTabBar={true}
+            extraHeight={200}
+            enableOnAndroid={true}
+        >
+            <FormContainer title={"Shipping Address"}>
+                <Input
+                    placeholder={"Phone"}
+                    name={"phone"}
+                    value={phone}
+                    keyboardType={"numeric"}
+                    onChangeText={(text) => setPhone(text)}
+                />
+                <Input
+                    placeholder={"Shipping Address 1"}
+                    name={"ShippingAddress1"}
+                    value={address}
+                    onChangeText={(text) => setAddress(text)}
+                />
+                <Input
+                    placeholder={"Shipping Address 2"}
+                    name={"ShippingAddress2"}
+                    value={address2}
+                    onChangeText={(text) => setAddress2(text)}
+                />
+                <Input
+                    placeholder={"City"}
+                    name={"city"}
+                    value={city}
+                    onChangeText={(text) => setCity(text)}
+                />
+                <Input
+                    placeholder={"Zip Code"}
+                    name={"zip"}
+                    value={zip}
+                    keyboardType={"numeric"}
+                    onChangeText={(text) => setZip(text)}
+                />
+                <Select
+                    width="80%"
+                    iosIcon={<Icon name="arrow-down" color={"#007aff"} />}
+                    style={{ width: undefined }}
+                    selectedValue={country}
+                    placeholder="Select your country"
+                    placeholderStyle={{ color: '#007aff' }}
+                    placeholderIconColor="#007aff"
+                    onValueChange={(e) => setCountry(e)}
+                >
+                    {countries.map((c) => {
+                        return <Select.Item
+                            key={c.code}
+                            label={c.name}
+                            value={c.name}
+                        />
+                    })}
+                </Select>
+
+                <View style={{ width: '80%', alignItems: "center" }}>
+                    <Button title="Confirm" onPress={() => checkOut()} />
+                </View>
+            </FormContainer>
+        </KeyboardAwareScrollView>
     )
 }
 export default Checkout;
