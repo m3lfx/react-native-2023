4894aba3b612db3a5cde8bc6ce668285248d94a5
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-08 23:07:01 +0800

    single product add to cart

--- Products/SingleProduct.js
+++ Products/SingleProduct.js
@@ -1,28 +1,41 @@
-import React, {useState, useEffect} from "react";
-import { Image, View, StyleSheet, Text, ScrollView, Button } from "react-native";
-import {Left, Right, Container, H1, Center} from 'native-base'
+import React, { useState, useEffect } from "react";
+import { Image, View, StyleSheet, Text, ScrollView, } from "react-native";
+import { Left, Right, Container, H1, Center, Button } from 'native-base'
+import { useDispatch } from 'react-redux'
+import * as actions from '../Redux/Actions/cartActions'
+import Toast from "react-native-toast-message";
 
-const SingleProduct = ({route}) => {
+const SingleProduct = ({ route }) => {
     const [item, setItem] = useState(route.params.item);
     console.log(item)
     const [availability, setAvailability] = useState('')
+    const dispatch = useDispatch()
 
     return (
         <Center flexGrow={1}>
-            <ScrollView style={{marginBottom: 80, padding: 5}}>
+            <ScrollView style={{ marginBottom: 80, padding: 5 }}>
                 <View>
                     {/* <Text>{item.name}</Text> */}
-                    <Image 
+                    <Image
                         source={{
                             uri: item.image ? item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
                         }}
                         resizeMode="contain"
                         style={styles.image}
                     />
-                        
+                    <Button size="sm" onPress={() => {
+                        dispatch(actions.addToCart({...item, quantity: 1, })),
+                            Toast.show({
+                                topOffset: 60,
+                                type: "success",
+                                text1: `${item.name} added to Cart`,
+                                text2: "Go to your cart to complete order"
+                            })
+                    }}>Add</Button>
+
                 </View>
             </ScrollView>
-        
+
         </Center>
     )
 }
@@ -31,7 +44,7 @@ const styles = StyleSheet.create({
     container: {
         position: 'relative',
         height: '100%',
-    
+
     },
     imageContainer: {
         backgroundColor: 'white',
