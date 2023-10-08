a3ae2cf4d193c5aba29225e38ca2ca2346c92144
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-08 20:04:20 +0800

    payment

--- Screens/Cart/Checkout/Payment.js
+++ Screens/Cart/Checkout/Payment.js
@@ -1,11 +1,114 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+import { View, Button, Pressable, FlatList, TouchableOpacity, Dimensions,  } from 'react-native'
 
-const Payment = () => {
+
+import {
+    Container,
+    Text,
+    Radio,
+    Right,
+    Left,
+    Picker,
+    Box,
+    HStack,
+    VStack,
+    Heading,
+    Divider,
+    CheckCircleIcon,
+    Select,
+    CheckIcon,
+    Center,
+
+} from 'native-base';
+
+import { useNavigation } from '@react-navigation/native';
+
+const methods = [
+    { name: 'Cash on Delivery', value: 1 },
+    { name: 'Bank Transfer', value: 2 },
+    { name: 'Card Payment', value: 3 }
+]
+
+const paymentCards = [
+    { name: 'Wallet', value: 1 },
+    { name: 'Visa', value: 2 },
+    { name: 'MasterCard', value: 3 },
+    { name: 'Other', value: 4 }
+]
+
+const Payment = (props) => {
+
+    // const order = props.route.params.order;
+    // console.log("order", order)
+
+    const [selected, setSelected] = useState('');
+    const [card, setCard] = useState('');
+    const navigation = useNavigation()
     return (
-        <View>
-            <Text> Payment</Text>
-        </View>
+        <Center  >
+            <Heading>
+                <Text>Choose your payment method</Text>
+            </Heading>
+
+            <HStack bg="red.200" width="100%"  >
+                <Radio.Group
+                    name="myRadioGroup"
+                    value={selected}
+                    onChange={(value) => {
+                        setSelected(value);
+                    }}
+
+                >
+                    {console.log(selected)}
+                    {methods.map((item, index) => {
+                        return (
+                            <Radio
+                                key={index}
+                                value={item.value} my="1"
+                                colorScheme="green"
+                                size="22"
+                                style={{ float: 'right' }}
+                                icon={<CheckCircleIcon size="22" mt="0.5" color="emerald.500" />}
+
+                            >
+                                {item.name}
+                            </Radio>
+                        )
+                    })
+                    }
+                </Radio.Group>
+            </HStack>
+            {selected === 3 ? (
+                <Box>
+                    <Select
+                        minWidth="100%"
+                        placeholder="Choose Service"
+                        selectedValue={card}
+                        onValueChange={(x) => setCard(x)}
+                        _selectedItem={{
+                            bg: "teal.600",
+                            endIcon: <CheckIcon size="5" />
+                          }}
+                    >
+                        {console.log(card)}
+                        {paymentCards.map((c, index) => {
+                            return (
+                                <Select.Item
+                                    key={c.name}
+                                    label={c.name}
+                                    value={c.name} />
+                            )
+                        })}
+
+                    </Select>
+                </Box>
+            ) : null}
+            <View style={{ marginTop: 60, alignSelf: 'center' }}>
+                <Button
+                    title={"Confirm"}
+                    onPress={() => navigation.navigate("Confirm", { order })} />
+            </View>
+        </Center>
     )
 }
 export default Payment;
