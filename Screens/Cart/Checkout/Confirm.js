347c6689926ee909d08d014c7713e944c6675340
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-16 14:29:24 +0800

    cnfirm order

--- Screens/Cart/Checkout/Confirm.js
+++ Screens/Cart/Checkout/Confirm.js
@@ -1,82 +1,150 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { View, StyleSheet, Dimensions, ScrollView, Button } from "react-native";
 import { Text, HStack, VStack, Avatar, Spacer, Center } from "native-base";
 
 import * as actions from "../../../Redux/Actions/cartActions";
 import Icon from 'react-native-vector-icons/FontAwesome'
+import Toast from "react-native-toast-message";
+import axios from "axios";
+import baseURL from "../../../assets/common/baseurl";
+import { useNavigation } from '@react-navigation/native';
+import { useSelector, useDispatch } from 'react-redux'
+import AsyncStorage from "@react-native-async-storage/async-storage"
+
 
 var { width, height } = Dimensions.get("window");
 
 const Confirm = (props) => {
-const confirm = props.route.params;
-console.log(confirm)
-    return (
-        <Center width={"90%"}>
-        <Text style={styles.title}>items</Text>
-              
-              {confirm ? confirm.order.order.orderItems.map((item) => {
-                return (
-                    // console.log(x)
-                    <HStack space={[2, 3]} justifyContent="space-between">
-                    <Avatar size="48px" source={{
-                        uri: item.image ? 
-                        item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
-                        }} 
-                    />
-                    <VStack>
-                        <Text _dark={{
-                        color: "warmGray.50"
+  const [token, setToken] = useState();
+  // const confirm = props.route.params;
+  const finalOrder = props.route.params;
+  console.log("order", finalOrder)
+  const dispatch = useDispatch()
+  let navigation = useNavigation()
+  
+  const confirmOrder = () => {
+    const order = finalOrder.order.order;
+    
+    AsyncStorage.getItem("jwt")
+      .then((res) => {
+        setToken(res)
+
+      })
+      .catch((error) => console.log(error))
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+    axios
+      .post(`${baseURL}orders`, order, config)
+      .then((res) => {
+        if (res.status == 200 || res.status == 201) {
+          Toast.show({
+            topOffset: 60,
+            type: "success",
+            text1: "Order Completed",
+            text2: "",
+          });
+          // dispatch(actions.clearCart())
+          // props.navigation.navigate("Cart")
+
+          setTimeout(() => {
+            dispatch(actions.clearCart())
+            navigation.navigate("Cart");
+          }, 500);
+        }
+      })
+      .catch((error) => {
+        Toast.show({
+          topOffset: 60,
+          type: "error",
+          text1: "Something went wrong",
+          text2: "Please try again",
+        });
+      });
+  }
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>Confirm Order</Text>
+        {props.route.params ? (
+          <View style={{ borderWidth: 1, borderColor: "orange" }}>
+            <Text style={styles.title}>Shipping to:</Text>
+            <View style={{ padding: 8 }}>
+              <Text>Address: {finalOrder.order.order.shippingAddress1}</Text>
+              <Text>Address2: {finalOrder.order.order.shippingAddress2}</Text>
+              <Text>City: {finalOrder.order.order.city}</Text>
+              <Text>Zip Code: {finalOrder.order.order.zip}</Text>
+              <Text>Country: {finalOrder.order.order.country}</Text>
+            </View>
+            <Text style={styles.title}>items</Text>
+
+            {finalOrder.order.order.orderItems.map((item) => {
+
+              return (
+
+                <HStack space={[2, 3]} justifyContent="space-between" key={item.id}>
+                  <Avatar size="48px" source={{
+                    uri: item.image ?
+                      item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
+                  }}
+                  />
+                  <VStack>
+                    <Text _dark={{
+                      color: "warmGray.50"
                     }} color="coolGray.800" bold>
-                        {item.name}
-                        </Text>
-                        <Text color="coolGray.600" _dark={{
-                    color: "warmGray.200"
-                    }}>
-                        {item.description}
-                        </Text>
-                    </VStack>
-                    <Spacer />
-                    <Text fontSize="xs" _dark={{
-                        color: "warmGray.50"
-                        }} color="coolGray.800" alignSelf="flex-start">
-                                {item.price}
+                      {item.name}
                     </Text>
-                    </HStack>
-                )
-              }): null}           
-            </Center>
 
-    )
+                  </VStack>
+                  <Spacer />
+                  <Text fontSize="xs" _dark={{
+                    color: "warmGray.50"
+                  }} color="coolGray.800" alignSelf="flex-start">
+                    {item.price}
+                  </Text>
+                </HStack>
+              )
+            })}
+          </View>
+        ) : null}
+        <View style={{ alignItems: "center", margin: 20 }}>
+          <Button title={"Place order"} onPress={confirmOrder} />
+        </View>
+      </View>
+    </ScrollView>
+  )
+
 }
-
 const styles = StyleSheet.create({
-    container: {
-      height: height,
-      padding: 8,
-      alignContent: "center",
-      backgroundColor: "white",
-    },
-    titleContainer: {
-      justifyContent: "center",
-      alignItems: "center",
-      margin: 8,
-    },
-    title: {
-      alignSelf: "center",
-      margin: 8,
-      fontSize: 16,
-      fontWeight: "bold",
-    },
-    listItem: {
-      alignItems: "center",
-      backgroundColor: "white",
-      justifyContent: "center",
-      width: width / 1.2,
-    },
-    body: {
-      margin: 10,
-      alignItems: "center",
-      flexDirection: "row",
-    },
-  });
+  container: {
+    height: height,
+    padding: 8,
+    alignContent: "center",
+    backgroundColor: "white",
+  },
+  titleContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 8,
+  },
+  title: {
+    alignSelf: "center",
+    margin: 8,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  listItem: {
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "center",
+    width: width / 1.2,
+  },
+  body: {
+    margin: 10,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+});
 export default Confirm;
--- assets/common/baseurl.js
+++ assets/common/baseurl.js
@@ -4,7 +4,7 @@ import { Platform } from 'react-native'
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://192.168.1.11:4000/api/v1/'
+? baseURL = 'http://192.168.1.3:4000/api/v1/'
 : baseURL = 'http://192.168.1.11:4000/api/v1/'
 }
 
