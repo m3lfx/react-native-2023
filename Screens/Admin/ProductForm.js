5bf575c73e009b3f288f5f560885e3b497d48f83
rommel dalisay <rommelmdalisay@gmail.com>
2023-10-16 11:53:56 +0800

    update product

--- Screens/Admin/ProductForm.js
+++ Screens/Admin/ProductForm.js
@@ -20,21 +20,24 @@ import Error from "../../Shared/Error"
 import axios from "axios"
 import * as ImagePicker from "expo-image-picker"
 import { useFocusEffect, useNavigation } from "@react-navigation/native"
+import mime from "mime";
 
 
 const ProductForm = (props) => {
+    // console.log(props.route.params)
+   
 
-    const [pickerValue, setPickerValue] = useState();
-    const [brand, setBrand] = useState();
-    const [name, setName] = useState();
-    const [price, setPrice] = useState();
-    const [description, setDescription] = useState();
-    const [image, setImage] = useState();
+    const [pickerValue, setPickerValue] = useState('');
+    const [brand, setBrand] = useState('');
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState(0);
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState('');
     const [mainImage, setMainImage] = useState();
-    const [category, setCategory] = useState();
+    const [category, setCategory] = useState('');
     const [categories, setCategories] = useState([]);
     const [token, setToken] = useState();
-    const [err, setError] = useState();
+    const [error, setError] = useState();
     const [countInStock, setCountInStock] = useState();
     const [rating, setRating] = useState(0);
     const [isFeatured, setIsFeature] = useState(false);
@@ -45,12 +48,27 @@ const ProductForm = (props) => {
     let navigation = useNavigation()
 
     useEffect(() => {
+        
+
+        if (!props.route.params) {
+            setItem(null);
+        } else {
+            setItem(props.route.params.item);
+            setBrand(props.route.params.item.brand);
+            setName(props.route.params.item.name);
+            setPrice(props.route.params.item.price.toString());
+            setDescription(props.route.params.item.description);
+            setMainImage(props.route.params.item.image);
+            setImage(props.route.params.item.image);
+            setCategory(props.route.params.item.category._id);
+            setPickerValue(props.route.params.item.category._id);
+            setCountInStock(props.route.params.item.countInStock.toString());
+        }
         AsyncStorage.getItem("jwt")
             .then((res) => {
                 setToken(res)
             })
             .catch((error) => console.log(error))
-
         axios
             .get(`${baseURL}categories`)
             .then((res) => setCategories(res.data))
@@ -84,6 +102,7 @@ const ProductForm = (props) => {
             setImage(result.assets[0].uri);
         }
     }
+    
 
     const addProduct = () => {
         if (
@@ -98,7 +117,7 @@ const ProductForm = (props) => {
         }
 
         let formData = new FormData();
-
+        const newImageUri = "file:///" + image.split("file:/").join("");
 
         formData.append("name", name);
         formData.append("brand", brand);
@@ -110,170 +129,206 @@ const ProductForm = (props) => {
         formData.append("rating", rating);
         formData.append("numReviews", numReviews);
         formData.append("isFeatured", isFeatured);
+        formData.append("image", {
+            uri: newImageUri,
+            type: mime.getType(newImageUri),
+            name: newImageUri.split("/").pop()
+        });
 
         const config = {
             headers: {
                 "Content-Type": "multipart/form-data",
-                Authorization: `Bearer ${token}`
+                "Authorization": `Bearer ${token}`
             }
         }
-        axios
-            .post(`${baseURL}products`, formData, config)
-            .then((res) => {
-                if (res.status == 200 || res.status == 201) {
+        if (item !== null) {
+            console.log(item)
+            axios
+                .put(`${baseURL}products/${item.id}`, formData, config)
+                .then((res) => {
+                    if (res.status == 200 || res.status == 201) {
+                        Toast.show({
+                            topOffset: 60,
+                            type: "success",
+                            text1: "Product successfuly updated",
+                            text2: ""
+                        });
+                        setTimeout(() => {
+                            navigation.navigate("Products");
+                        }, 500)
+                    }
+                })
+                .catch((error) => {
                     Toast.show({
                         topOffset: 60,
-                        type: "success",
-                        text1: "New Product added",
-                        text2: ""
-                    });
-                    setTimeout(() => {
-                        navigation.navigate("Products");
-                    }, 500)
-                }
-            })
-            .catch((error) => {
-                console.log(error)
-                Toast.show({
-                    topOffset: 60,
-                    type: "error",
-                    text1: "Something went wrong",
-                    text2: "Please try again"
+                        type: "error",
+                        text1: "Something went wrong",
+                        text2: "Please try again"
+                    })
                 })
-            })
+        } else {
+            axios
+                .post(`${baseURL}products`, formData, config)
+                .then((res) => {
+                    if (res.status == 200 || res.status == 201) {
+                        Toast.show({
+                            topOffset: 60,
+                            type: "success",
+                            text1: "New Product added",
+                            text2: ""
+                        });
+                        setTimeout(() => {
+                            navigation.navigate("Products");
+                        }, 500)
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                    Toast.show({
+                        topOffset: 60,
+                        type: "error",
+                        text1: "Something went wrong",
+                        text2: "Please try again"
+                    })
+                })
 
-        return (
-            <FormContainer title="Add Product">
-                <View style={styles.imageContainer}>
-                    <Image style={styles.image} source={{ uri: mainImage }} />
-                    <TouchableOpacity
-                        onPress={pickImage}
-                        style={styles.imagePicker}>
-                        <Icon style={{ color: "white" }} name="camera" />
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.label}>
-                    <Text style={{ textDecorationLine: "underline" }}>Brand</Text>
-                </View>
-                <Input
-                    placeholder="Brand"
-                    name="brand"
-                    id="brand"
-                    value={brand}
-                    onChangeText={(text) => setBrand(text)}
-                />
-                <View style={styles.label}>
-                    <Text style={{ textDecorationLine: "underline" }}>Name</Text>
-                </View>
-                <Input
-                    placeholder="Name"
-                    name="name"
-                    id="name"
-                    value={name}
-                    onChangeText={(text) => setName(text)}
-                />
-                <View style={styles.label}>
-                    <Text style={{ textDecorationLine: "underline" }}>Price</Text>
-                </View>
-                <Input
-                    placeholder="Price"
-                    name="price"
-                    id="price"
-                    value={price}
-                    keyboardType={"numeric"}
-                    onChangeText={(text) => setPrice(text)}
-                />
-                <View style={styles.label}>
-                    <Text style={{ textDecorationLine: "underline" }}>Count in Stock</Text>
-                </View>
-                <Input
-                    placeholder="Stock"
-                    name="stock"
-                    id="stock"
-                    value={countInStock}
-                    keyboardType={"numeric"}
-                    onChangeText={(text) => setCountInStock(text)}
-                />
-                <View style={styles.label}>
-                    <Text style={{ textDecorationLine: "underline" }}>Description</Text>
-                </View>
-                <Input
-                    placeholder="Description"
-                    name="description"
-                    id="description"
-                    value={description}
-                    onChangeText={(text) => setDescription(text)}
-                />
-                <Box>
-                    <Select
-                        minWidth="90%" placeholder="Select your Category"
-                        selectedValue={pickerValue}
-                        onValueChange={(e) => [setPickerValue(e), setCategory(e)]}
-                    >
-                        {categories.map((c, index) => {
-                            return (
-                                <Select.Item
-                                    key={c.id}
-                                    label={c.name}
-                                    value={c.id} />
-                            )
-                        })}
-
-                    </Select>
-                </Box>
-
-                {err ? <Error message={err} /> : null}
-                <View style={styles.buttonContainer}>
-                    <EasyButton
-                        large
-                        primary
-                        onPress={() => addProduct()}
-                    ><Text style={styles.buttonText}>Confirm</Text>
-                    </EasyButton>
-                </View>
-            </FormContainer>
-        )
-    }
-
-    const styles = StyleSheet.create({
-        label: {
-            width: "80%",
-            marginTop: 10
-        },
-        buttonContainer: {
-            width: "80%",
-            marginBottom: 80,
-            marginTop: 20,
-            alignItems: "center"
-        },
-        buttonText: {
-            color: "white"
-        },
-        imageContainer: {
-            width: 200,
-            height: 200,
-            borderStyle: "solid",
-            borderWidth: 8,
-            padding: 0,
-            justifyContent: "center",
-            borderRadius: 100,
-            borderColor: "#E0E0E0",
-            elevation: 10
-        },
-        image: {
-            width: "100%",
-            height: "100%",
-            borderRadius: 100
-        },
-        imagePicker: {
-            position: "absolute",
-            right: 5,
-            bottom: 5,
-            backgroundColor: "grey",
-            padding: 8,
-            borderRadius: 100,
-            elevation: 20
         }
-    })
 
-    export default ProductForm;
+    }
+    return (
+        <FormContainer title="Add Product">
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={{ uri: mainImage }} />
+                <TouchableOpacity
+                    onPress={pickImage}
+                    style={styles.imagePicker}>
+                    <Icon style={{ color: "white" }} name="camera" />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.label}>
+                <Text style={{ textDecorationLine: "underline" }}>Brand</Text>
+            </View>
+            <Input
+                placeholder="Brand"
+                name="brand"
+                id="brand"
+                value={brand}
+                onChangeText={(text) => setBrand(text)}
+            />
+            <View style={styles.label}>
+                <Text style={{ textDecorationLine: "underline" }}>Name</Text>
+            </View>
+            <Input
+                placeholder="Name"
+                name="name"
+                id="name"
+                value={name}
+                onChangeText={(text) => setName(text)}
+            />
+            <View style={styles.label}>
+                <Text style={{ textDecorationLine: "underline" }}>Price</Text>
+            </View>
+            <Input
+                placeholder="Price"
+                name="price"
+                id="price"
+                value={price}
+                keyboardType={"numeric"}
+                onChangeText={(text) => setPrice(text)}
+            />
+            <View style={styles.label}>
+                <Text style={{ textDecorationLine: "underline" }}>Count in Stock</Text>
+            </View>
+            <Input
+                placeholder="Stock"
+                name="stock"
+                id="stock"
+                value={countInStock}
+                keyboardType={"numeric"}
+                onChangeText={(text) => setCountInStock(text)}
+            />
+            <View style={styles.label}>
+                <Text style={{ textDecorationLine: "underline" }}>Description</Text>
+            </View>
+            <Input
+                placeholder="Description"
+                name="description"
+                id="description"
+                value={description}
+                onChangeText={(text) => setDescription(text)}
+            />
+            <Box>
+                <Select
+                    minWidth="90%" placeholder="Select your Category"
+                    selectedValue={pickerValue}
+                    onValueChange={(e) => [setPickerValue(e), setCategory(e)]}
+                >
+                    {categories.map((c, index) => {
+                        return (
+                            <Select.Item
+                                key={c.id}
+                                label={c.name}
+                                value={c.id} />
+                        )
+                    })}
+
+                </Select>
+            </Box>
+
+            {error ? <Error message={error} /> : null}
+            <View style={styles.buttonContainer}>
+                <EasyButton
+                    large
+                    primary
+                    onPress={() => addProduct()}
+                ><Text style={styles.buttonText}>Confirm</Text>
+                </EasyButton>
+            </View>
+        </FormContainer>
+    )
+}
+
+
+const styles = StyleSheet.create({
+    label: {
+        width: "80%",
+        marginTop: 10
+    },
+    buttonContainer: {
+        width: "80%",
+        marginBottom: 80,
+        marginTop: 20,
+        alignItems: "center"
+    },
+    buttonText: {
+        color: "white"
+    },
+    imageContainer: {
+        width: 200,
+        height: 200,
+        borderStyle: "solid",
+        borderWidth: 8,
+        padding: 0,
+        justifyContent: "center",
+        borderRadius: 100,
+        borderColor: "#E0E0E0",
+        elevation: 10
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        borderRadius: 100
+    },
+    imagePicker: {
+        position: "absolute",
+        right: 5,
+        bottom: 5,
+        backgroundColor: "grey",
+        padding: 8,
+        borderRadius: 100,
+        elevation: 20
+    }
+})
+
+
+export default ProductForm;
